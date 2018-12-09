7d770760ba460b1a45412d205bc32c116a430e8f
Thomas Lieser <tlieserMN@gmail.com>
2018-12-09 12:46:12 -0600

    Basic web structure complete

--- DemoWebsite/Controllers/HomeController.cs
+++ DemoWebsite/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DemoWebsite.Models;
 
 namespace DemoWebsite.Controllers
 {
@@ -10,21 +11,7 @@ namespace DemoWebsite.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View("Home", TEMPDATA.HOME);
         }
     }
 }
