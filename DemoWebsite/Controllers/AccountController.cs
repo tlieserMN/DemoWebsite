e043e3dd0cdfccd075a8179e7874d39b1b599b05
Thomas Lieser <tlieserMN@gmail.com>
2018-12-09 13:56:26 -0600

    New Files

---
+++ DemoWebsite/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWebsite.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Account()
+        {
+            return View("Account", TEMPDATA.LOGEDINUSER);
+        }
+    }
+}
---
+++ DemoWebsite/Globals/SessionVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoWebsite.Globals
+{
+    public static class SessionVariables
+    {
+        public static string CurrentUser = TEMPDATA.LOGEDINUSER.FirstName + " " + TEMPDATA.LOGEDINUSER.LastName;
+        public static int CurrentUserID = TEMPDATA.LOGEDINUSER.ID;
+    }
+}
